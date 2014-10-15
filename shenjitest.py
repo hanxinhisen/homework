1747c634c688a4ea0ee079348ca1e037c95da051
hanxin_hisen <hanxin_hisen@yeah.net>
2014-10-15 15:57:56 +0800

    modified:   audit_server_hx.sql 	modified:   demo.log 	modified:   interactive.pyc 	modified:   shenji.py 	modified:   shenjitest.py

--- shenjitest.py
+++ shenjitest.py
@@ -13,7 +13,7 @@ from multiprocessing import Process, Pool
 import paramiko
 import sys,os
 import paramiko
-host='192.168.1.107'
+host='172.16.110.1'
 port=22
 user='root'
 password='123456'
@@ -21,7 +21,12 @@ password='123456'
 s = paramiko.SSHClient()	#绑定实例
 s.load_system_host_keys()	#加载本机HOST主机文件
 s.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+time1=time.time()
 s.connect(host,port,user,password,timeout=5)   #连接远程主机
+time2=time.time()
+print '++++++++++++++++++++++++'
+print time2-time1
+print '++++++++++++++++++++++++'
 stdin,stdout,stderr = s.exec_command('hostname')		#执行
 cmd_result = stdout.read(),stderr.read()		#读取命令
 print '\033[32;1m-------------%s-----------\033[0m' % host, user
